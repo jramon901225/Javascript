7156012e662e38da5a2f224807a32b451bd12f90
jramon901225 <jramon901225@gmail.com>
2021-10-10 15:09:53 -0500

    tarea de funciones tipo flecha

---
+++ 06-funciones tipo flecha/js/script.js
@@ -0,0 +1,247 @@
+//ejercicio de invertir un string
+
+let palabra = prompt('ingrese una palabra')
+let cambio = palabra.split("")
+let cambio2 = ((cambio.reverse()).join("")).toString()
+console.log(palabra.split(""))
+console.log(palabra.split("").reverse().join(""))
+
+
+//Ejercicio Shopping cart
+
+
+
+const cartShoppings = [
+    {
+        name: 'Coca lata 355ml',
+        description: 'Coca lata 355ml',
+        price: 11,
+        type: 'drink'
+    },
+    {
+        name: 'Boing Mango 500ml',
+        description: 'Boing Mango 500ml',
+        price: 10,
+        type: 'drink'
+    },
+    {
+        name: 'Boing Mango 1lt',
+        description: 'Boing Mango 1lt',
+        price: 18,
+        type: 'drink'
+    },
+    {
+        name: 'Coca-Cola 3lt No Retornable',
+        description: 'Coca-Cola 3lt No Retornable',
+        price: 40,
+        type: 'drink'
+    },
+    {
+        name: 'Coca-Cola 600ml',
+        description: 'Coca-Cola 600ml',
+        price: 14,
+        type: 'drink'
+    },
+    {
+      name: 'Donitas Bimbo Espolvoreadas 105 g + Pan Dulce Donas Bimbo Azucaradas 105 g\t',
+      description: 'Donitas Bimbo Espolvoreadas 105 g + Pan Dulce Donas Bimbo Azucaradas 105 g\t',
+      price: 27.5,
+      type: 'chip'
+    },
+    {
+      name: 'Donitas Bimbo Espolvoreadas 105 g + Pan Dulce Nito Con Chocolate 62 g\t\n',
+      description: 'Donitas Bimbo Espolvoreadas 105 g + Pan Dulce Nito Con Chocolate 62 g\t\n',
+      price: 25.5,
+      type: 'chip'
+    },
+    {
+      name: 'Botanas Chips Papas Jalapeño 100 g',
+      description: 'Botanas Chips Papas Jalapeño 100 g',
+      price: 29.7,
+      type: 'chip'
+    },
+    {
+      name: 'Botana Paketaxo Quexo 215 g',
+      description: 'Botana Paketaxo Quexo 215 g',
+      price: 52.8,
+      type: 'chip'
+    },
+    {
+      name: 'Botana Doritos Incógnita 64 g',
+      description: 'Botana Doritos Incógnita 64 g',
+      price: 18.15,
+      type: 'chip'
+    },
+    {
+      name: 'Botana Fritos Sal y Limón 57 g',
+      description: 'Botana Fritos Sal y Limón 57 g',
+      price: 15.4,
+      type: 'chip'
+    },
+    {
+      name: 'Botana Doritos Nacho 58 g',
+      description: 'Botana Doritos Nacho 58 g',
+      price: 17.6,
+      type: 'chip'
+    },
+    {
+      name: 'Botana Doritos Flamin Hot 62 g',
+      description: 'Botana Doritos Flamin Hot 62 g',
+      price: 17.6,
+      type: 'chip'
+    },
+    {
+      name: 'Botanas Chips Papas Fuego Limón 100 g',
+      price: 29.7,
+      type: 'chip'
+    },
+    {
+      name: 'Papas Sabritas Original 42 g',
+      description: 'Papas Sabritas Original 42 g',
+      price: 17.6,
+      type: 'chip'
+    },
+    {
+      name: 'Papas Sabritas Adobadas 42 g',
+      description: 'Papas Sabritas Adobadas 42 g',
+      price: 16.5,
+      type: 'chip'
+    },
+    {
+      name: 'Cacahuates Hot Nuts Multintenso 50 g',
+      description: 'Cacahuates Hot Nuts Multintenso 50 g',
+      price: 11,
+      type: 'chip'
+    },
+    {
+      name: 'Botana Fritos Chorizo Chipotle 57 g',
+      description: 'Botana Fritos Chorizo Chipotle 57 g',
+      price: 15.95,
+      type: 'chip'
+    },
+    {
+      name: 'Botana Chips Sal de Mar Bolsa 170 g',
+      description: 'Botana Chips Sal de Mar Bolsa 170 g',
+      price: 49.5,
+      type: 'chip'
+    },
+    {
+      name: 'Botana Cheetos Poffs 100 g',
+      description: 'Botana Cheetos Poffs 100 g',
+      price: 34.1,
+      type: 'chip'
+    },
+    {
+      name: 'Papas Fritas Stax Cheddar 156 g',
+      description: 'Papas Fritas Stax Cheddar 156 g',
+      price: 47.85,
+      type: 'chip'
+    },
+    {
+      name: 'Papas Sabritas Original 240 g',
+      description: 'Papas Sabritas Original 240 g',
+      price: 75.35,
+      type: 'chip'
+    },
+    {
+      name: 'Botana Doritos Nacho 370 g',
+      description: 'Botana Doritos Nacho 370 g',
+      price: 82.5,
+      type: 'chip'
+    },
+    {
+      name: 'Chicharrón de Cerdo Sabritas 115 g',
+      description: 'Chicharrón de Cerdo Sabritas 115 g',
+      price: 55,
+      type: 'chip'
+    },
+    {
+      name: 'Botana Cheetos Torciditos 150 g',
+      description: 'Botana Cheetos Torciditos 150 g',
+      price: 34.1,
+      type: 'chip'
+    },
+    {
+      name: 'Papas Ruffles Queso 290 G',
+      description: 'Papas Ruffles Queso 290 G',
+      price: 71.5,
+      type: 'chip'
+    },
+    {
+      name: 'Dip Tostitos Queso Suave 255 g',
+      description: 'Dip Tostitos Queso Suave 255 g',
+      price: 69.3,
+      type: 'chip'
+    }
+  ]
+
+  const shoppingCartFunction = (cartShoppings, productType) => {
+    const coincidencias = [];
+    cartShoppings.forEach((product) => {
+        if(product.type === productType) {
+            coincidencias.push(product)
+        }
+    });
+    console.log(coincidencias)
+    return coincidencias
+  }
+
+shoppingCartFunction(cartShoppings, "chip")
+shoppingCartFunction(cartShoppings, "drink")
+
+
+const searchProductpriceLess50 = (cartShoppings, productPrice) => {
+    const coincidencias = [];
+    cartShoppings.forEach((product) => {
+        if(product.price < productPrice) {
+            coincidencias.push(product)
+        }
+    });
+    console.log(coincidencias)
+    return coincidencias
+  }
+
+  searchProductpriceLess50(cartShoppings, 50)
+
+  const searchProductpriceGreater50 = (cartShoppings, productPrice) => {
+    const coincidencias = [];
+    cartShoppings.forEach((product) => {
+        if(product.price >= productPrice) {
+            coincidencias.push(product)
+        }
+    });
+    console.log(coincidencias)
+    return coincidencias
+  }
+  searchProductpriceGreater50(cartShoppings, 50)
+
+  
+
+  function totalPrice(data){
+    let coincidencia = []
+    let suma = 0
+    data.forEach((product)=>{
+      suma += product["price"]
+    }) 
+  console.log(suma)
+}
+totalPrice(cartShoppings)
+
+
+function totalPrice(data,type = null){
+    let suma = 0
+    data.forEach((product)=>{
+      if(product["type"] === type) {
+        suma += product["price"]
+      }
+      if(type === null){
+        suma += product["price"]
+      }
+    }) 
+  console.log(suma)
+}
+
+totalPrice(cartShoppings)
+totalPrice(cartShoppings,"chip")
+totalPrice(cartShoppings,"drink")
+
