ddc82322070b34fbcff64aee32c2f27611ebf9ee
jramon901225 <jramon901225@gmail.com>
2021-10-22 06:55:32 -0500

    tarea DOM

---
+++ 01-DOM/js/script.js
@@ -0,0 +1,140 @@
+const mentorsArray = [
+    {
+        name:"Ivan Diaz",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:8
+            },
+            {
+                signature:"ReactJS",
+                score:8
+            }
+        ]
+    },
+    {
+        name:"Alan Medina",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:7
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:10
+            }
+        ]
+    },
+    {
+        name:"Elvira Camarillo",
+        scores:[
+            {
+                signature:"HTML",
+                score:9
+            },
+            {
+                signature:"CSS",
+                score:9
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:9
+            }
+        ]
+    },
+    {
+        name:"Alejandra Paez",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:9
+            },
+            {
+                signature:"ReactJS",
+                score:10
+            }
+        ]
+    },
+  ]
+    /*
+    -Crear thead de la tabla
+    -iterar un arreglo que tenga los heads
+    -crear un row
+    -crear un th por cada elemento de ese arreglo                                 
+    -Crear Tbody de la tabla
+
+    */
+  let tablaMentor = document.getElementById("mentores"); 
+  let tablebody = document.createElement('tbody') 
+
+  mentorsArray.forEach( Mentor => { 
+
+    let row = document.createElement('tr'); 
+    let td = document.createElement('td'); 
+    let avg = 0
+
+                /*Iteramos nombres*/
+
+    td.innerText = Mentor.name; 
+    row.appendChild(td); 
+
+                /*Iteramos Materias*/
+     
+    Mentor.scores.forEach((scores) =>{
+
+     td = document.createElement('td')
+     td.innerText = scores.score
+     row.appendChild(td); 
+   })
+
+                  /*Iteramos Promedio*/
+
+     Mentor.scores.forEach((scores) => {
+     td = document.createElement('td')
+     avg += scores.score /Mentor.scores.length
+     td.innerText = avg 
+   })
+    row.appendChild(td)
+    tablaMentor.appendChild(tablebody);
+    tablebody.appendChild(row);
+
+                /* Creamos Botones */
+
+   const button = document.createElement('button')
+   button.type = 'button'
+   button.innerText = 'click'
+   row.appendChild(button)
+   tablebody.appendChild(row)
+  })
+  
+  button.classlist.add('boton');
+    
+
---
+++ 14-funciones_constructoras/js/script.js
@@ -0,0 +1,8 @@
+function Persona (name) {
+this.name = name;
+}
+
+Persona()
+
+const person1 = new Persona ('Manuel')
+console.log(person1)
